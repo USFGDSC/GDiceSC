de23ec5d4d4015f43aa22757ded509bf16bd8abf
RobinSardja <robinsardja@gmail.com>
2024-01-19 21:02:52 -0500

    Standard app layout.

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,54 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+  int _currentIndex = 0;
+  final PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: "GDiceSC",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text( "GDiceSC" ),
+          centerTitle: true,
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: (newIndex) => setState( () => _currentIndex = newIndex ),
+          children: const [
+            Center( child: Text( "Home") ),
+            Center( child: Text( "Settings") ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (newIndex) {
+            setState( () => _currentIndex = newIndex );
+            _pageController.jumpToPage(_currentIndex);
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon( Icons.home ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon( Icons.settings ),
+              label: "Settings",
+            ),
+          ],
         ),
       ),
     );
